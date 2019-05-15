4a7214113f80d3efb3cfcf9ab5f108d3c51405b7
melike <melike.goksoy@cantekgroup.com>
2019-05-15 17:27:57 +0300

    versiyonlandi

--- projects/c-energy-bar-chart/src/lib/barchart/barchart.component.ts
+++ projects/c-energy-bar-chart/src/lib/barchart/barchart.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { single } from './data';
+import { ToolboxComponent } from '../toolbox-decorator';
+
+ @ToolboxComponent({
+   desc: ' Energy Bar Chart',
+   icon: 'fa fa-chart-bar',
+   componentName : 'BarchartComponent'
+ })
 @Component({
   selector: 'app-barchart',
   templateUrl: './barchart.component.html',
